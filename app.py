baa11355fdb8fc3f61286405c93d6e288a328d4f
Brian Ringler <ringler.brian.8286@gmail.com>
2020-11-06 17:00:10 -0500

    added udpate and delete. Still need to fix css issue not pulling in

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,8 +18,46 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # content is name of input that we data from
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding item to db'
+    else:
+        # grab all tasks in the db ordered by date created
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return f'There was a problem with deleting id: {id}'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+    
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return f'There was and issue with the update for id: {id}'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ =='__main__':
     app.run(debug=True)
