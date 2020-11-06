4aae2dd5718d4c108afa3ea644a325437ad3593d
Brian Ringler <ringler.brian.8286@gmail.com>
2020-11-06 05:59:08 -0500

    added static, templates and db

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datatime import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Todo(db.model):
-    id = db.column(db.Integer, priamry_key=True)
-    content = db.Column(db.string(200), nullable=False)
-    completed = db.Column(db.integer, default=0)
-    data_created - db.column(db.DateTime, default=datetime.utcnow)
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Task: {self.id}>'
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
